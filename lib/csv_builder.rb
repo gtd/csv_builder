b79e42d8c75fcb3f2ea71623f821d7c052423e51
Michael Reinsch <michael@mobalean.com>
2010-11-26 19:10:04 +0900

    - fixed several errors - split it up - really make it work with Rails 3 (should also still work with Rails 2)

--- lib/csv_builder.rb
+++ lib/csv_builder.rb
@@ -1,72 +1,17 @@
 # encoding: utf-8
 
-if RUBY_VERSION.to_f >= 1.9
-  require 'csv'
-  FCSV = CSV
-else
-  require 'fastercsv'
-  FCSV = FasterCSV
+module CsvBuilder
+  if RUBY_VERSION.to_f >= 1.9
+    require 'csv'
+    CSV_LIB = CSV
+  else
+    require 'fastercsv'
+    CSV_LIB = FasterCSV
+  end
 end
 
 require 'iconv'
-require 'transliterating_filter'
+require 'csv_builder/transliterating_filter'
+require 'csv_builder/template_handler'
+require 'csv_builder/railtie'
 
-module ActionView # :nodoc:
-  module Template
-    module Handlers
-    # Template handler for csv templates
-    #
-    # Add rows to your CSV file in the template by pushing arrays of columns into csv
-    #
-    #   # First row
-    #   csv << [ 'cell 1', 'cell 2' ]
-    #   # Second row
-    #   csv << [ 'another cell value', 'and another' ]
-    #   # etc...
-    #
-    # You can set the default filename for that a browser will use for 'save as' by
-    # setting <tt>@filename</tt> instance variable in your controller's action method
-    # e.g.
-    #
-    #   @filename = 'report.csv'
-    #
-    # You can also set the input encoding and output encoding by setting
-    # <tt>@input_encoding</tt> and <tt>@output_encoding</tt> instance variables.
-    # These default to 'UTF-8' and 'LATIN1' respectively. e.g.
-    #
-    #   @output_encoding = 'UTF-8'
-    class CsvBuilder < ActionView::Template::Handler
-
-      def compile(template)
-        <<-EOV
-        begin
-          output = FCSV.generate(@csv_options || {}) do |faster_csv|
-            csv = TransliteratingFilter.new(faster_csv, @input_encoding || 'UTF-8', @output_encoding || 'LATIN1')
-            #{template.source}
-          end
-
-          unless defined?(ActionMailer) && defined?(ActionMailer::Base) && controller.is_a?(ActionMailer::Base)
-            @filename ||= "\#{controller.action_name}.csv"
-            if controller.request.env['HTTP_USER_AGENT'] =~ /msie/i
-              controller.response.headers['Pragma'] = 'public'
-              controller.response.headers["Content-type"] = "text/plain" 
-              controller.response.headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
-              controller.response.headers['Content-Disposition'] = "attachment; filename=\#{@filename}"
-              controller.response.headers['Expires'] = "0" 
-            else
-              controller.response.headers["Content-Type"] ||= 'text/csv'
-              controller.response.headers["Content-Disposition"] = "attachment; filename=\#{@filename}"
-              controller.response.headers["Content-Transfer-Encoding"] = "binary"
-            end
-          end
-
-          output
-        rescue Exception => e
-          Rails.logger.warn("Exception \#{e} \#{e.message} with class \#{e.class.name} thrown when rendering CSV")
-          raise e
-        end
-        EOV
-      end
-    end
-  end
-end
--- lib/csv_builder/transliterating_filter.rb
+++ lib/csv_builder/transliterating_filter.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
-class TransliteratingFilter
+class CsvBuilder::TransliteratingFilter
   # Transliterate into the required encoding if necessary
   def initialize(faster_csv, input_encoding = 'UTF-8', output_encoding = 'LATIN1')
     @faster_csv = faster_csv
