8575ea0eb3e0bdbbce3c821d2af93b1da8a761cc
Gabe da Silveira <gabe@websaviour.com>
2011-04-22 17:18:28 -0700

    Fix the remaining spec

--- spec/controllers/csv_builder_spec.rb
+++ spec/controllers/csv_builder_spec.rb
@@ -12,7 +12,7 @@ class CsvBuilderReportsController < ApplicationController
       format.csv
     end
   end
-  
+
   def complex
     respond_to do |format|
       format.csv do
@@ -22,13 +22,13 @@ class CsvBuilderReportsController < ApplicationController
       end
     end
   end
-  
+
   def encoding
     respond_to do |format|
       format.csv { @output_encoding = 'UTF-16' }
     end
   end
-  
+
 end
 ActionController::Routing::Routes.draw { |map| map.connect ':controller/:action' }
 
@@ -41,25 +41,25 @@ describe CsvBuilderReportsController do
       get 'simple'
       response.should be_success
     end
-    
+
     it "responds to CSV" do
       get 'simple', :format => 'csv'
       response.should be_success
     end
   end
-  
+
   describe "Layout with options" do
     it "sets output encoding correctly" do
       get 'encoding', :format => 'csv'
-      correct_output = generate({}, Iconv.iconv('UTF-16//TRANSLIT//IGNORE', 'UTF-8', 'ąčęėįšųūž'))
+      correct_output = generate({}, [Iconv.iconv('UTF-16//TRANSLIT//IGNORE', 'UTF-8', 'ąčęėįšųūž')])
       response.body.to_s.should == correct_output
     end
-    
+
     it "passes csv options" do
       get 'complex', :format => 'csv'
       response.body.to_s.should == generate({ :col_sep => "\t" })
     end
-    
+
     it "sets filename" do
       get 'complex', :format => 'csv'
       response.headers['Content-Disposition'].should match(/filename=some_complex_filename.csv/)
