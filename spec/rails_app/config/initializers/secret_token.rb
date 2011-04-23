df04e0982581aaa547a570d94c0fc1ab5a527b50
Gabe da Silveira <gabe@websaviour.com>
2011-04-22 22:28:39 -0700

    Remove cruft from test app

--- spec/rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_token = '8386121b064dad2dd497b21385b64eebd7da5a38f6aba13fe4f3035f90f2c3f414e632a8de1a0db7552e3cff6a402bc513142f869521c037500efea39196a6aa'
