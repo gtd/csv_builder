919b59879085759e8de1a650278259e97a8c1bde
Joel Chippindale <joel.chippindale@econsultancy.com>
2012-05-24 20:46:07 +0100

    Use accessors in CsvBuilder::TransliteratingFilter

--- lib/csv_builder/transliterating_filter.rb
+++ lib/csv_builder/transliterating_filter.rb
@@ -2,8 +2,8 @@
 
 class CsvBuilder::TransliteratingFilter
   # Transliterate into the required encoding if necessary
-  def initialize(faster_csv, input_encoding = 'UTF-8', output_encoding = 'LATIN1')
-    @faster_csv = faster_csv
+  def initialize(csv, input_encoding = 'UTF-8', output_encoding = 'LATIN1')
+    self.csv = csv
 
     # TODO: do some checking to make sure iconv works correctly in
     # current environment. See ActiveSupport::Inflector#transliterate
@@ -11,13 +11,16 @@ class CsvBuilder::TransliteratingFilter
     #
     # Not using the more standard //IGNORE//TRANSLIT because it raises
     # Iconv::IllegalSequence for some inputs
-    @iconv = Iconv.new("#{output_encoding}//TRANSLIT//IGNORE", input_encoding) if input_encoding != output_encoding
+    self.iconv = Iconv.new("#{output_encoding}//TRANSLIT//IGNORE", input_encoding) if input_encoding != output_encoding
   end
 
   # Transliterate before passing to FasterCSV so that the right characters (e.g. quotes) get escaped
   def <<(row)
-    @faster_csv << if @iconv then row.map { |value| @iconv.iconv(value.to_s) } else row end
+    csv << if iconv then row.map { |value| iconv.iconv(value.to_s) } else row end
   end
 
   alias :add_row :<<
+
+  private
+  attr_accessor :csv, :iconv
 end
